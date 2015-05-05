f6b7f67a378581f04d4e6dbc747b4d3ee3fd9ba5
sbalazote <sbalazote@gmail.com>
2015-05-05 03:34:03 -0300

    #agrego clase CalendarUtils

---
+++ src/com/accountingapp/utils/CalendarUtils.java
@@ -0,0 +1,32 @@
+package com.accountingapp.utils;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class CalendarUtils {
+
+	private static CalendarUtils instance = null;
+	
+	public static CalendarUtils getInstance() {
+		if (instance == null) {
+			return new CalendarUtils();
+		} else {
+			return instance;
+		}
+	}
+	
+	protected CalendarUtils() {
+		
+	}
+	
+	public String getLastDayOfMonth(Date date) {
+		Calendar cal = new GregorianCalendar(date.getYear()+1900, date.getMonth()+1, 0);
+		Date newDate = cal.getTime();
+		DateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		System.out.println("Date : " + sdf.format(newDate));
+		return sdf.format(newDate);
+	}
+}
